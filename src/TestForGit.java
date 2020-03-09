07ca3d98a413314a1a229184c3e9b675bcf68e63
woshige <37682973+woshige@users.noreply.github.com>
2020-03-09 20:09:21 +0800

    Update TestForGit.java

--- src/TestForGit.java
+++ src/TestForGit.java
@@ -1,7 +1,7 @@
 public class TestForGit {
     public static void main(String[] args) {
         System.out.println("这是我的本地修改");
-        System.out.println("这是我的第一次远端修改");
+        System.out.println("this is my frist remote change");
         System.out.println("这是我的第二次远端修改");
     }
 }
